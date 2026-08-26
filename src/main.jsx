b84427e85f261ef3ffe000d5c92e4e73bea184b3
AdolfoRomero10 <adolfo.romero@friaai.com>
2026-08-26 00:32:30 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,6 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet-async'
+import './index.css'
 import './responsive.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </StrictMode>,
-)
+createRoot(document.getElementById('root')).render(<App />)
